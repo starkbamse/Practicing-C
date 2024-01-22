0190b780ba189f7788d23930073cec445755331d
arumeel <kaisa.arumeel@gmail.com>
2024-01-22 11:06:59 +0100

    Finished exercises 1-3

--- wp-1/e2.c
+++ wp-1/e2.c
@@ -1,63 +1,74 @@
 // Exercise 2
 
 // Include section
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <ctype.h> 
 
 // Main function in the program
 
 /*
-* Purpose: 
+* Purpose: This is an encryption program that “shifts characters” in the ASCII-code table of a given
+* input string. The user inputs the number of characters to shift as well as the input text.
+* The program outputs the encrypted text.
 *
 * Authors: Kaisa Arumeel, Alexander Säfström, Omid Khodaparast
 */
+
+// The main function
 int main(int argc, char *argv[]) {
 
-    int limit,
-    i,
-    ascii;
-    char insertedText[limit],
-    encryptedText[limit];
+    // Variable declarations
+    int shift,  // Number of characters to shift
+    i,          // Index of the loop
+    ascii,      // Ascii value of the current character
+    result;     // Ascii value of the encrypted character
+    char insertedText[50];  // User inputted text
 
+    // Check for the provided argument to shift characters.
     if (argc != 2){
-       printf("Please enter the number of characters you wish to encrypt \n");
+       printf("Please enter the number of characters you wish to shift. \n");
        return 0;
     }
 
-    limit = atoi(argv[1]);
+    // Convert the provided argument into an integer.
+    shift = atoi(argv[1]);
 
-    do
-    {
-       printf("Type a text to encrypt with maximum length of %i \n", limit);
-       scanf("%s", insertedText);
+    do {
+    
+    // Ask the user to type a text and save the input.
+    printf("\nType a text to encrypt: ");
+    scanf("%s", insertedText);
 
-       for (i = 0; i < limit; i++)
-       {
-        ascii = (int)insertedText[i];
-            // if current character is within a and m
-            if (ascii >= 97 && ascii <=  109){
-                // add "13" to ascii code to get encrypted character and store in solution char array
-                encryptedText[i] = (char) (ascii + 13);
-            //if current character is within n and z
-            } else if (ascii >= 110 && ascii <= 122){
-                // substract "13" to ascii code to get encrypted character and store in solution char array
-                encryptedText[i] = (char) (ascii - 13);
-            // if current character is within A and M
-            } else if (ascii >= 65 && ascii <= 77){
-                // substract "13" to ascii code to get encrypted character and store in solution char array
-                encryptedText[i] = (char) (ascii + 13);
-            // if current character is within N and Z
-            } else if (ascii >= 78 && ascii <= 90){
-                // substract "13" to ascii code to get encrypted character and store in solution char array
-                encryptedText[i] = (char) (ascii - 13);
-            }else {
-                // if not a letter, leave it as it is
-                encryptedText[i] = insertedText[i];
-            }
-       }
-       printf("%s\n", encryptedText);
-       
-    }  while ((strcmp(insertedText, "^Z") != 0) || (strcmp(insertedText, "^d") != 0));
+    // Loop through each character in the inputted text.
+    for (i = 0; i < strlen(insertedText); i++) {
+    // Save the ascii value of the current character as an integer.
+    ascii = (int)insertedText[i];
+    // Get the encrypted character ascii value by adding the shift number.
+    result = ascii + shift;
+    // Check if the current character is lower-case letter.
+    if(islower(insertedText[i])){ 
+        // Check if the encrypted character is more than 122, so the lower-case z in ASCII table
+        if(result > 122) {
+            // Then substract 26, so the encrypted character would stay lower-case and go back a round in the alphabet.
+            result = result - 26; 
+        } 
+    } 
+    // Check if the current character is upper-case letter.
+    if (isupper(insertedText[i])) {
+    // Check if the encrypted character is more than 90, so the upper-case Z in ASCII table
+        if(result > 90) { 
+            // Then substract 26, so the encrypted character would stay upper-case and go back a round in the alphabet.
+            result = result - 26; 
+        } 
+    } 
+    // Show the encrypted letter to the user
+    printf("%c", result);
+    }
+    
+    // Do this as long as the EOF character is detected.
+    // feof() method finds EOF characters and stdin means in the standard input.
+    }  while (!feof(stdin));
     
 }
--- wp-1/e3.c
+++ wp-1/e3.c
@@ -12,62 +12,80 @@
 // Main function in the program
 
 /*
-* Purpose: 
+* Purpose: This program generates a random number between 1 and 100.
+* The user can guess the number and the program responds whether the guess
+* is too low, high or correct. There is a set number of maximum guesses.
+* After the game has finished, the user can either play again or exit the program.
 *
 * Authors: Kaisa Arumeel, Alexander Säfström, Omid Khodaparast
 */
 
+
+// Main function
 int main(int argc, char *argv[]) {
 
+// Initalise the random generation of numbers so that it does not return the same number every execution 
 srand ( time(NULL) );
-//Create a random number from (0 - 99 as the modulo %100) + 1
+//Create a random number from between 1 and 100 (included).
 int randomNumber =  rand() %100 + 1;
 
-
-int input, 
-playAgain,
-numberOfGuesses = 0;
+// Variable declarations
+int input,           // User input
+playAgain,           // Flag for whether the user wants to play again
+numberOfGuesses = 0; // Counter for number of made guesses
 
 do
 {
+   // Ask user for a number and save the input
    printf("Enter a number between 1 and 100: \n");
    scanf("%d", &input);
    printf("%d", input);
+   // If the input is not in the range, ask for a valid input.
     if ( input > 100 || input < 1) {
         printf("Please enter a valid integer between 1 and 100.\n");
-        continue; // Restart the loop if the input is not valid
     }
+    // Guess is lower than the random number
     else if(input < randomNumber) {
         printf("Your guess is too low.\n");
+        // Increase number of guesses count
         numberOfGuesses += 1;
     }
+    // Guess is higher that the random number
     else if(input > randomNumber) {
         printf("Your guess is too high.\n");
-         numberOfGuesses += 1;
+        // Increase number of guesses count
+        numberOfGuesses += 1;
     }
+    // Guess is correct
     else {
+        // Increase number of guesses count
         numberOfGuesses += 1;
         printf("You have guessed %d times and your guess is correct.\n", numberOfGuesses);
+        // Ask whether the user wants to play again
         printf("Press 1 to play again, 0 to finish the game: ");
         scanf("%d", &playAgain);
-
+        // If the user pressed 1, create a new random number and reset the number of guesses counter.
         if (playAgain == 1) {
             randomNumber = rand() % 100 + 1;
             numberOfGuesses = 0;
         }
     }
 
-} while (input != randomNumber && numberOfGuesses < MAX_NUMBER);
-
+    // User has guessed maximum number of times.
     if(numberOfGuesses == MAX_NUMBER) {
         printf("%s\n", "You have run out of tries. \n");
+        // Ask whether the user wants to play again
         printf("Press 1 to play again, 0 to finish the game: ");
         scanf("%d", &playAgain);
+        // If the user pressed 1, create a new random number and reset the number of guesses counter.
         if (playAgain == 1) {
             randomNumber = rand() % 100 + 1;
             numberOfGuesses = 0;
         }
     }
+// Do this as long as the guess is not correct and the number of guesses does not exceed maximum number of tries.
+} while (input != randomNumber && numberOfGuesses < MAX_NUMBER);
+
 
 return 0;
 }
