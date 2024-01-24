6351c9cb90e99e0a09764a427b3e0919335eeb37
alesaf <gussafala@student.gu.se>
2024-01-24 17:56:53 +0100

    Added template, and wp-1

---
+++ template.c
@@ -0,0 +1,22 @@
+//Include section
+//#include <stdio.h>
+
+//Define section
+//#define MAX 5
+
+//Main program section
+
+/**
+* This program is a useful program because it is used
+* in very useful situations, therefore you should
+* use it.
+* 
+* Purpose: Being useful is a good trait
+* DIT632
+* 
+* Author: A useful person, 2024
+*
+* Note: all variable decl at the top of each method.
+* define where the program logic starts.
+* 
+**/
---
+++ wp-1/e1.c
@@ -0,0 +1,55 @@
+//Include section
+#include <stdio.h>
+
+//Define section
+#define MAX 5
+
+//Main program section
+
+/**
+* This program reads an integer between 1 and 5 and
+* prints out one of 5 previously defined sentences.
+* 
+* Purpose: To showcase how conditionals are used in C.
+* DIT632
+* 
+* Author: Kaisa Arumeel, Omid Khodaparast, Alexander Säfström
+*
+**/
+
+int main(int argc, char* argv[]) {
+
+    //variable declarations
+
+    //array with pointers that point to the first char in the string.
+    char* sentences[]={
+        "1. Lorem ipsum dolor sit amet, adipiscing elit.",
+        "2. Duis at luctus bob augue.",
+        "3. Ut vehicula magna metus, nulla placerat eu.",
+        "4. Etiam vulputate volutpat id maximus.",
+        "5. In lorem lectus, vel, posuere et justo."}; 
+
+    int selectedSentence; //number to store the selected sentence
+    
+    //program logic
+
+    do {
+        //Prompt the user to enter a number
+        printf("Please enter a number from 1 to 5.\n");
+
+        //Read decimal input from stdin and store 
+        //it at the memory address of selectedSentence
+        scanf("%d",&selectedSentence);
+
+        //Check if number is within interval before accessing array
+        if(selectedSentence<=5 && selectedSentence>=1){
+            //Print the selected sentence to stdout.
+            printf("%s\n",sentences[selectedSentence-1]);
+        }
+
+        //We keep repeating this loop until the input
+        //is outside of the interval.
+    } while(selectedSentence<=5 && selectedSentence>=1);
+
+    return 0;
+}
