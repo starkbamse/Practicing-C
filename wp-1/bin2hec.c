d7723cae71a22d4512037f5da0c4fb162413df90
alesaf <gussafala@student.gu.se>
2024-01-25 19:37:16 +0100

    bin2hec completed

--- wp-1/bin2hec.c
+++ wp-1/bin2hec.c
@@ -6,9 +6,10 @@
 //Define section
 #define INITIAL_MEMORY_ALLOCATION 5
 #define MIN_CHAR 48 // The number 0 in the ASCII table
-#define MAX_CHAR 57 // The number 9 in the ASCII table
+#define MAX_CHAR 49 // The number 1 in the ASCII table
 #define BYTE_LENGTH 8 // The number of bits in a byte
 #define BASE 10 // The defined input base 
+
 
 int validateInput(int* argc,char*argv[]); // Used to validate user input
 
@@ -31,12 +32,12 @@ int main(int argc, char* argv[]) {
 
     // Variable declarations
     size_t allocation=INITIAL_MEMORY_ALLOCATION; //Init alloc for conv. result
-    char* result =malloc(INITIAL_MEMORY_ALLOCATION); // Alloc mem for conv. result
-    long userInput; // Users input converted to long
+    char* result = malloc(INITIAL_MEMORY_ALLOCATION); // Alloc mem for conv. result
+    char* userInput; // Users input converted to long
     int position=0; // Cursor or position in the conversion result.
     int inputOk; // Is input valid or not
     int bitsLeft; // Number of bits left in the byte.
-    int conversionLength; // The resulting length of the conversion
+    int inputLength; // The user input length
     
     // Program logic 
 
@@ -59,12 +60,25 @@ int main(int argc, char* argv[]) {
     // Convert the argument to a long
     // We have already performed validation so
     // We pass NULL to the end_ptr and set base 10.
-    userInput=strtol(argv[1],NULL,BASE);
+    userInput=argv[1];
 
-    // While the users input is not 0
-    while (userInput!=0){
-        // We check if one addition to the string literal
-        // will overrun the allocated memory.
+    // Setting input length to be the length of user input
+    inputLength=strlen(userInput);
+
+    // Loop through the entire input, increment by groups
+    // of 4 or the length of a byte/.
+    for(int i=0;i<inputLength;i+=BYTE_LENGTH/2){
+        int number;
+        for(int x=i;x<BYTE_LENGTH/2;x++){
+            if(x<inputLength){
+                if(userInput[x]=='1'){
+                    number = number | 1;
+                }
+                if((x+1)<inputLength){
+                    number=number << 1;
+                }
+            }
+        }
         if((strlen(result)+1)>allocation){
             // Increase the allocation by 2.
             allocation*=2;
@@ -78,45 +92,15 @@ int main(int argc, char* argv[]) {
             // Exit with exit code 2.
             return 2;
         }
-
-        // Perform bitwise operation AND on 
-        // userInput and 1. Since 1's LSB is 1,
-        // it will require userInput's LSB be to also
-        // be 1.
-        if(userInput & 1) {
-            // Set the current position of result to 1
-            result[position]='1';
+        if(number<10){
+            result[position]='0'+number;
         } else {
-            // Set the current position of result to 0
-            result[position]='0';
+            result[position]=('F'-(15-number));
         }
-        // Shift userInput one bit to the right.
-        userInput=userInput>>1;
-        // Increment position.
         position++;
     }
 
-    // The length of the resulting binary string.
-    conversionLength=strlen(result);
-
-    // To ensure we add padding where needed,
-    // we check how many slots in a slot of 8 are overflowing
-    // by performing the modulus operation, and then subtracting
-    // the result from 8, which tells us how many zeros we need
-    // to prepend the result by.
-    bitsLeft=8-(conversionLength%8);
-
-    // Prepend the result for how many bits we need.
-    for(int i=0;i<bitsLeft;i++){
-        // Prepend with 0.
-        printf("0");
-    }
-    // Since we worked with LSBs we start at the end of the string literal
-    // to get the correct order of the bits.
-    for(int i=(conversionLength-1);i>=0;i--){
-        // Print the character.
-        printf("%c",result[i]);
-    }
+    printf("%s",result);
     // Finalize the string by adding a newline.
     printf("\n");
 }
