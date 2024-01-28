9a75b584ef6385a6018b1af37ae5271ec74e86a5
alesaf <gussafala@student.gu.se>
2024-01-28 20:14:58 +0100

    Update e5
    Co-Authored-By: Omid <omidkhoda0101@gmail.com>

--- wp-1/e5.c
+++ wp-1/e5.c
@@ -1,103 +1,96 @@
-//Include section
-#include <stdio.h>
+// (C) __Omid Khodaparast, Alexander Säfström, Kaisa Arumeel, group: 2 __ (2024)
+// Work package 1
+// Exercise 5
+// Submission code: xxxxxx (provided by your TA-s)
+
+// Include section
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
 
-//Define section
 #define MAX 100 // Defines the maximum number of the values in the table
 #define MAXNUMBER 20 // Defines the maximum value of random numbers
 
-//Main program section
-
-/**
-* This program uses two initial values, a table size and a maximum
-* number, and creates a randomized table with different numbers.
-* The program counts the frequency of these numbers and plots
-* them on a histogram.
-* 
-* Purpose: To discover the distribution of numbers generated
-* by a pseudo-RNG using the current system time as the seed.
-* DIT632
-* 
-* Author: Kaisa Arumeel, Omid Khodaparast, Alexander Säfström
-* 
-**/
-
-// ------ Function declarations ----------
-
+// ------ Function declarations ------
 // This function generates a set of random numbers
 // and fills the table *tab with these numbers
-void create_random(int *tab );
+void create_random(int *tab);
 // This function takes the *tab of random numbers
 // and creates a table with the frequency counts for these numbers
-void count_frequency(int *tab, int *freq );
+void count_frequency(int *tab, int *freq);
 // This function takes the frequency count table
 // and draws a histogram of the values in that frequency table
-void draw_histogram(int *freq );
-// Validates the user arguments
-int validateInput(int*argc,char**argv[]);
+void draw_histogram(int *freq);
 
-// ------ Function definitions ----------
+// ------ Function definitions ------ 
 
-// ------ Main --------------------------
+// ---------- main ------------------
 // The main entry point for the program
-int main (int argc, char* argv[]) {
-    //Variable declarations
-    int table[MAX]; // An array of random integers.
+int main (void) {
+    int table[MAX]; // construct an array with size MAX
+    int frequency[MAXNUMBER + 1]; // construct an array with size MAXNUMBER + 1 (from 0 (including) to MAXNUMBER (including))
     
-    // An array storing the number of occurrences of each number in the table.
-    int frequency[MAXNUMBER]; 
-    
-    time_t currentTime=time(NULL); // Current system time.
+    memset(frequency,0,sizeof(frequency));
 
-    // Program logic
-
-    // Seed the random number generator.
-    srand(currentTime);
-    // Set frequency in table to 0.
-    memset(frequency, 0, sizeof(frequency));
-    // Create a randomized table
     create_random(table);
-    // Count the frequency of each number
-    count_frequency(table,frequency);
-    // Draw a histogram of the table.
+    count_frequency(table, frequency);
     draw_histogram(frequency);
+
+    // rtuen 0 upon program finish
+    return 0;
 }
 
-void create_random(int *tab){
-    // Loop from 0 to the maximum value, the size of table.
-    for(int i=0;i<MAX;i++){
-        // Set the current element's value to a random value modulus
-        // the maximum number that we have set to be generated.
-        *(tab+i)=rand()%(MAXNUMBER+1);
+void create_random(int *tab) {
+    int randNum;  // variable to hold random number between 1 and 100 (included)
+
+    // seed the randomiser
+    srand(time(0));
+
+    // now that we have the size we can assign random numbers
+    for (int i = 0; i < MAX; i++) {
+        // get modulo of MAXNUMBER and add 1 to get a numb between 0 and MAXNUMBER
+        randNum = (rand() % (MAXNUMBER + 1));
+        // set undex i value to randNum
+        tab[i] = randNum;
+        // printf("tab[%d] = %d\n", i, randNum);
     }
 }
 
-void count_frequency(int *tab,int*freq) {
-    // Loop from 0 to the maximum value, the size of table.
-    for(int i=0;i<MAX;i++){
-        // Increment the frequency of the element that is
-        // the value of the element stored in tab+i.
-        *(freq+*(tab+i))+=1;
+void count_frequency(int *tab, int *freq) {
+    int randNum; // this variable holds the random number whihc will be used to find the index in freq
+
+
+
+
+    // go through the random number array and count the frequency of each number
+    for (int i = 0; i < MAX; i++) {
+        // get the numb at index i
+        randNum = tab[i];
+        // for this number between 0 and MAXNUMBER add 1 to what it was before
+        freq[randNum]++;
     }
 }
 
-void draw_histogram(int*freq){
-    // Loop from 0 to the maximum value that we can generate.
-    for(int i=0;i<MAXNUMBER+1;i++){
-        // Check if the frequency is bigger than 0.
-        if(*(freq+i)>0){
-            // Print the number.
-            printf("%2d ",i);
-            // Loop from 0 to the frequency of this number
-            for(int x=0;x<*(freq+i);x++){
-                // Print an x to stdout.
-                printf("x");
-            }
-            // Print a newline.
-            printf("\n");
+void draw_histogram(int *freq) {
+    int freqPerNum; // this variable holds frequency per number
+
+    // go t through the array. If the freq is not 0 draw
+    for (int i = 0; i <= MAXNUMBER; i++) {
+        // if freq is 0, do not draw and continue to the next iteration
+        // printf("freq[%d] = %d\n", i, freq[i]);
+        if (freq[i] == 0) continue;
+        // set freq for the current index
+        freqPerNum = freq[i];
+        // print the num taking into consideration that it can need two spaces on the terminal
+        printf("%2d   ", i);
+
+        // print x corresponding to the frequency of the current number
+        for (int j = 1; j <= freqPerNum; j++) {
+            printf("x");
         }
+
+        // go to the next line
+        printf("\n");
     }
 }
