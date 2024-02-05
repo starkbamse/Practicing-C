c5b5e1ac524e8f919d1532f22e665e4a80992273
arumeel <kaisa.arumeel@gmail.com>
2024-02-05 13:47:24 +0100

    Add exercice 2

--- wp-2/e2.c
+++ wp-2/e2.c
@@ -1,140 +1,150 @@
-//Include section
+// (C) __Omid Khodaparast, Alexander Säfström, Kaisa Arumeel, group: 2 __ (2024)
+// Work package 2
+// Exercise 2
+// Submission code: 
+
+
+// Include section
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-//Define section
-#define MAX 5 // Maximum nodes in linked list
-#define MAX_RANDOM 100 // Maximum random value
+
+//#### Constants #####
+#define MAX 5 // Maximum number of elements in the linked list.
+#define MAX_VALUE 100 // Maximum value of an element in the linked list.
+
 
 /**
-* This program creates a random linked list of a specified size
-* that is filled with random numbers generated within a specified
-* interval. The contents of this linked list is then printed, and 
-* finally a new element is placed on the head of the linked list.
-* 
-* Purpose: To showcase how a complex data structure can be created
-* through the use of pointers and structs in C.
+* This program generates a random linked list with 5 elements. The values of each element
+* are between 0 and 100. 
+* The program also adds a new record to the first position of the list and prints out both, the original and the new linked list.
+*
+* Purpose: Linked list
 * DIT632
 * 
 * 
 **/
 
+
 // ##### typedefs ####
-typedef struct q{ 
-    int number; // Number stored in element of linked list.
-    struct q *next; // Next node.
-    struct q *prev; // Previous node.
+// Defining the element in the linked list.
+typedef struct q{
+int number; // The value of the element-
+struct q *next; // Pointer to the next element in the linked list.
+struct q *prev; // Pointer to the previous element in the linked list.
 } REGTYPE;
+
 // ##### Function declarations #####
-
-// Creates a linked list and fills it with random numbers.
 REGTYPE* random_list(void);
-
-// Takes a value and creates a new node and places it
-// on the head of the linked list.
 REGTYPE* add_first(REGTYPE* temp, int data);
+
 
 //###### Main program #######
 int main(int argc, char *argv[])
 {
-    int nr=0; // Counter for current position in linked list.
-    // act_post is the current node, whilst head is
-    // the first element in the list.
+    // Variable declarations
+    int nr=0; // Number of the element.
+    // Act_post and head will both store the first node of the linked list.
     REGTYPE *act_post , *head=NULL; 
+
     srand(time(0)); // Random seed
-    head=random_list(); // Create a random linked list.
-    act_post=head; // Set to point to first element.
-    
-    // While we still have a node that pointed to a non-null node.
+
+    // Create the random list and assign it to head.
+    head=random_list();
+    // Assign act_post to head.
+    act_post=head;
+
+    // Loop through the linked list.
     while( act_post!=NULL){
-        // Print to stdout.
-        printf("\n Post nr %d : %d" , nr++, act_post->number);
-        // Update the current node to be node that the current
-        // node points to.
-        act_post=act_post->next;
+    // Print the current element of the linked list.
+    printf("\n Post nr %d : %d" , nr++, act_post->number);
+   // Print the current element of the linked list.
+    act_post=act_post->next;
     }
 
-    // Create a new node and make it the first element,
-    // and set the value to 123.
-    head=add_first(head,123);
 
-    // Print to stdout. This prints new head value as well as what the
-    // new head points to, the old head.
-    printf("\nHead - value: %d points to value: %d\n",head->number,head->next->number);
+    printf("\n\nAdding a new node...\n");
+    // Add a new node with a random value to the linked list.
+    head = add_first(head, rand()% (MAX_VALUE + 1));
+    // Assign act_post to head.
+    act_post = head;
+    // Set the number back to 0.
+    nr = 0;
+
+    // Loop through the linked list
+    while( act_post!=NULL){
+    // Print the current element of the linked list.
+    printf("\n Post nr %d : %d" , nr++, act_post->number);
+    // Print the current element of the linked list.
+    act_post=act_post->next;
+    }
+
+    printf("\n");
+
 
     // --- Free the allocated memory ---
-    while((act_post=head)!=NULL){ 
-        // Update the head to point to the node that the current head
-        // points to.
-        head=act_post->next;
-        free(act_post); // Free occupied heap memory.
+    while((act_post=head)!=NULL){
+    head=act_post->next;
+    free(act_post);
     }
-    // Exit gracefully.
+    
     return 0;
 }
 // ==== End of main ======================================
+
+// Create a linked list with random values.
 REGTYPE* random_list(void ){
+int i; // Indec of the loop
+REGTYPE *top, *old, *item; // Declare REGTYPE variables and set them to NULL.
 
-    int nr, i=0; // Index and number value
+// Loop from 0 to the max value (5)
+    for (i= 0; i < MAX; i++){
+        // Declare a new node and allocate memory
+        item = malloc(sizeof(REGTYPE));
+        // Set the number of the node to a random value between 0 and 100.
+        item -> number = rand()%(MAX_VALUE + 1);
 
-    REGTYPE *top, *old, *item; // Three empty nodes.
-    
-    // Allocate memory on heap for node of struct size.
-    top=malloc(sizeof(REGTYPE));
-
-    // A head cannot have a previous node.
-    top->prev=NULL;
-
-    // Generate a random number
-    nr=rand()%(MAX_RANDOM+1);
-
-    // Update the value of the head node.
-    top->number=nr;
-
-    // Mark the top as the old node.
-    old=top;
-
-    // From 0 until the max
-    for (i=0;i<MAX;i++){
-        // Allocate memory on heap for node of struct size.
-        item=malloc(sizeof(REGTYPE));
-
-        // Ensure next is pointing to a null pointer.
-        item->next=NULL; 
-
-        // Generate a new random number
-        nr=rand()%(MAX_RANDOM+1);
-
-        // Update the number of the new node.
-        item->number=nr;
-
-        // Set the current old's next node to be the 
-        // newly created node
-        old->next=item;
-
-        // Now, the current new node becomes the old node.
-        old=item;
+    // Check if it is the first element in the linked list.
+        if (i == 0)
+        {
+        // Then there is no top or old node, so put them all to equal each other.
+           top = old = item;
+        }
+        // If it is not the first element in the linked list.
+        else {
+            // Set the current item's previous value to point to the old item.
+             item->prev = old;
+             // Set the old item's next to point to the current item.
+             old->next = item;
+             // Set the old item to equal the current item.
+             old = item;
+        }
     }
-    
-    // Return the head of the list.
-    return(top);
+    // Once we are at the last element, there is no next, so we assign it to NULL.
+    old->next = NULL;
+// Return the first element in the linked list.
+return(top);
 }
-//==========================================================
+
+// This function adds a new element to the first position of the linked list.
+// It takes in as parameters the pointer to the first element in a linked list and a value.
 REGTYPE* add_first(REGTYPE* temp, int data){
-    // Allocate memory for the new node.
-    REGTYPE *new=malloc(sizeof(REGTYPE));
 
-    // Assume the new node is already the head,
-    // so point to the old head.
-    new->next=temp;
+    // Declare a new node.
+    REGTYPE *item;
+    // Assign memory to the new node.
+    item = malloc(sizeof(REGTYPE));
 
-    // Update the number of the new head to the argument.
-    new->number=data;
-
-    // Make the temp pointer point to the new head.
-    temp=new;
-
-    // Return new head.
-    return temp;
+    // Set the previous of the first element of the linked list to point to the new node.
+    temp->prev = item;
+    // As the new node is the first element in the linked list, it does not have a previous element.
+    // So we set the previous to point to NULL.
+    item->prev = NULL;
+    // Set the new node's next to point to the previous first element.
+    item ->next = temp;
+    // Set the value of the new node to the value passed in from parameters.
+    item->number = data;
+    // Return a pointer to the new element.
+    return(item);
 }
