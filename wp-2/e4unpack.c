0a0f6ff9cce3b18a30ab71a7027a1c832325b347
Omid Khodaparast <guskhodom@student.gu.se>
2024-02-05 11:44:12 +0100

    Done with e4 part 2

--- wp-2/e4unpack.c
+++ wp-2/e4unpack.c
@@ -22,9 +22,9 @@ typedef struct {
 // --- Function declarations ---
 // Unpack the hexadecimal value provided as an argument
 // Then the function prints the unpacked values
-void unpack(char *packedByte);
+void unpack(unsigned char *packedByte);
 // Validate the argument provided
-void validateArg(int argc, char *argv[]);
+int validateArg(int argc, char *argv[]);
 // --- Function declarations ---
 
 /**
@@ -34,11 +34,85 @@ void validateArg(int argc, char *argv[]);
 
 // Main function, where the programe starts
 int main(int argc, char *argv[]) {
+    unsigned char *hexrep; // Variable to hold the hex representation
 
+    // If validation fails, return 1 to indicate error
+    if (!validateArg(argc, argv)) {
+        return 1;
+    }
+
+    hexrep = argv[1];
+    unpack(hexrep);
     // Return 0 upon programe finish to indicate success
     return 0;
 }
 
-void unpack(char *packedByte) {
+void unpack(unsigned char *packedByte) {
+    unsigned char packed; // Variable to hold the packed hexadecimal
+    CAR unpacked; // Variable to hold the unpacked values
+    int decimal; // Variable to hold the decimal value of the hex chars
     
+    // Convert the hex into decimal
+    decimal = strtoul(packedByte, NULL, 16);
+    // Set the packed unsigned char
+    packed = packed | decimal;
+
+    // Get the value for break two, LSB. Use bitwise &
+    unpacked.breakTwo = 1 & packed;
+    // Shift 1 to the right to get rid of the current bit and go to the second bit
+    packed >>= 1;
+    // Get the value for break 1
+    unpacked.breakOne = 1 & packed;
+    // Shift one to the right to get rid of the current bit
+    packed >>= 1;
+    // Get keyPos value. 3 is used as keyPos could have a max of 0b11
+    unpacked.keyPos = 3 & packed;
+    // Shift packed to right by 2 bits as keyPos took 2 bits
+    packed >>= 2;
+    // Get gearPos value by comparing to 7 as the max value of gearPos is 111
+    unpacked.gearPos = 7 & packed;
+    // Shift to the right 3 bits as gearPos took 3 bits
+    packed >>= 3;
+    // Get engineOn value
+    unpacked.engineOn = 1 & packed;
+
+    // Print the unpacked values
+    printf("Name           Value\n");
+    printf("-----------------------------\n");
+    printf("engine_on      %d\n", unpacked.engineOn);
+    printf("gear_pos       %d\n", unpacked.gearPos);
+    printf("key_pos        %d\n", unpacked.keyPos);
+    printf("brake1         %d\n", unpacked.breakOne);
+    printf("brake2         %d\n", unpacked.breakTwo);
+}
+
+int validateArg(int argc, char *argv[]) {
+    
+    // If the arguments provided is less than or greater than 2, print error message and return 0 to indicate failure in validation
+    if (argc < 2 || argc > 2) {
+        printf("You must provide one argument.\n");
+        return 0;
+    }
+
+    // If the length of the provided arg is greater than 2, whihc means that the value is greater than 256 (8 bytes), 
+    // print error message and return 0 to indicate failure in validation
+    if (strlen(argv[1]) > 2) {
+        printf("The value provided is too big, enter a value between 0 and 255 in hex notation please.\n");
+        return 0;
+    }
+
+    // Check that the chars conform to hex notation
+    for (int i = 0; i < strlen(argv[1]); i++) {
+        // ASCII 0 = 48
+        // ASCII 9 = 57
+        // ASCII A = 65
+        // ASCII F = 70
+        // if the char ASCII val is less than 48 or greater than 57 (given that it is not between 65 and 70), print error message and return 0
+        if (argv[1][i] < 48 || (argv[1][i] > 57 && (argv[1][i] < 65 || argv[1][i] > 70))) {
+            printf("The value provided does not conform to hex notation\n");
+            return 0;
+        }
+    }
+
+    return 1;
 }
